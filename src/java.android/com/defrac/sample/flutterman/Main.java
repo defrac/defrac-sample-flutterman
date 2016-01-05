bb70964179ed1b706c5f846009acf054fd841b4b
Joa Ebert <joa.ebert@defrac.com>
2016-01-05 09:52:03 +0100

    Use onBeforeCreateContentView callback

--- src/java.android/com/defrac/sample/flutterman/Main.java
+++ src/java.android/com/defrac/sample/flutterman/Main.java
@@ -5,13 +5,13 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.View;
 
 import defrac.dni.Activity;
 import defrac.dni.IntentFilter;
 import defrac.dni.UsesSdk;
 import defrac.ui.Screen;
 import defrac.ui.ScreenActivity;
-import defrac.ui.View;
 
 @Activity(
     label = "Flutterman",
@@ -20,12 +20,12 @@ import defrac.ui.View;
 @UsesSdk(minSdkVersion = Build.VERSION_CODES.ICE_CREAM_SANDWICH)
 public final class Main extends ScreenActivity {
   @Override
-  public void onCreate(Bundle savedInstanceState) {
+  protected void onBeforeCreateContentView() {
+    super.onBeforeCreateContentView();
+
     requestWindowFeature(Window.FEATURE_NO_TITLE);
     getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
         WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-    super.onCreate(savedInstanceState);
   }
 
   @Override
@@ -40,12 +40,12 @@ public final class Main extends ScreenActivity {
     if (hasFocus) {
       if (android.os.Build.VERSION.SDK_INT >= 19) {
         getWindow().getDecorView().setSystemUiVisibility(
-            android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-            android.view.View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-            android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-            android.view.View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-            android.view.View.SYSTEM_UI_FLAG_FULLSCREEN |
-            android.view.View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+            View.SYSTEM_UI_FLAG_FULLSCREEN |
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
       }
     }
   }
