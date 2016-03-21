bc43b1324d9a23bc4416ff10c833c6cb292ca546
Joa Ebert <joa.ebert@defrac.com>
2016-03-21 14:20:42 +0100

    Update for 0.3.116

--- src/java.android/com/defrac/sample/flutterman/Main.java
+++ src/java.android/com/defrac/sample/flutterman/Main.java
@@ -9,7 +9,6 @@ import android.view.View;
 
 import defrac.dni.Activity;
 import defrac.dni.IntentFilter;
-import defrac.dni.UsesSdk;
 import defrac.ui.Screen;
 import defrac.ui.ScreenActivity;
 
@@ -17,7 +16,6 @@ import defrac.ui.ScreenActivity;
     label = "Flutterman",
     screenOrientation = "landscape",
     filter = @IntentFilter(action = Intent.ACTION_MAIN, category = Intent.CATEGORY_LAUNCHER))
-@UsesSdk(minSdkVersion = Build.VERSION_CODES.ICE_CREAM_SANDWICH)
 public final class Main extends ScreenActivity {
   @Override
   protected void onBeforeCreateContentView() {
